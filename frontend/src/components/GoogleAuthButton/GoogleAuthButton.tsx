919020a3a2874e7c9134e9b32c2d132b9ef73b38
merry <k.maria.m.24.05@gmail.com>
2023-12-06 16:42:37 +0200

    add button

---
+++ frontend/src/components/GoogleAuthButton/GoogleAuthButton.tsx
@@ -0,0 +1,13 @@
+const GoogleAuthButton = () => {
+  const handleClick = () => {
+    const oauth2Endpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Sign in with Google</button>
+    </div>
+  );
+};
+
+export default GoogleAuthButton;
