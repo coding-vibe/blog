21f509f9fea403b1b719dd3075f87ea8d5151d59
merry <k.maria.m.24.05@gmail.com>
2023-12-06 22:02:30 +0200

    add authpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import GoogleAuthButton from "./components/GoogleAuthButton";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AuthPage from "./pages/AuthPage";
+import Layout from "./components/Layout";
 import "./App.css";
 
+const routesMap = createBrowserRouter([
+  {
+    element: <Layout />,
+    path: "/",
+    children: [
+      {
+        element: <AuthPage />,
+        path: "/auth",
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <GoogleAuthButton />;
+  return <RouterProvider router={routesMap} />;
 }
 
 export default App;
---
+++ src/pages/AuthPage/AuthPage.tsx
@@ -0,0 +1,27 @@
+export default function AuthPage() {
+  const handleClick = () => {
+    const oauth2Endpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+    const params = {
+      client_id:
+        "931479452772-cvdr2ksv1ruvs0c0thg6jvs4dqk153t4.apps.googleusercontent.com",
+      redirect_uri: "http://localhost:3000/verify-auth",
+      response_type: "token",
+      scope: "email profile",
+    };
+    const authUrl =
+      `${oauth2Endpoint}?` +
+      Object.entries(params)
+        .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
+        .join("&");
+    window.open(authUrl);
+
+    const responseUrl = window.location.href;
+    console.log(responseUrl);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Sign in with Google</button>
+    </div>
+  );
+}
