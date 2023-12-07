dc0c320a02cf8fbbea0f4d08ce694813d0377315
merry <k.maria.m.24.05@gmail.com>
2023-12-07 17:16:31 +0200

    fix comments

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,20 +1,22 @@
+import queryString from "query-string";
+
 const CLIENT_ID = import.meta.env.VITE_AUTH_CLIENT_ID;
 const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
 
 export default function AuthPage() {
   const handleClick = () => {
-    const oauth2Endpoint = "https://accounts.google.com/o/oauth2/v2/auth";
-    const params = {
+    const url = "https://accounts.google.com/o/oauth2/v2/auth";
+    const query = {
       client_id: CLIENT_ID,
       redirect_uri: REDIRECT_URI,
       response_type: "token",
       scope: "email profile",
     };
-    const authUrl =
-      `${oauth2Endpoint}?` +
-      Object.entries(params)
-        .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
-        .join("&");
+    const authUrl = queryString.stringifyUrl({
+      url,
+      query,
+    });
+
     window.open(authUrl);
   };
 
--- src/pages/AuthVerificationPage/AuthVerificationPage.tsx
+++ src/pages/AuthVerificationPage/AuthVerificationPage.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import queryString from "query-string";
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 const CLIENT_ID = import.meta.env.VITE_VERIFY_CLIENT_ID;
@@ -9,58 +10,53 @@ const GET_ARTICLES_URL = `${BASE_URL}/articles`;
 
 export default function AuthVerificationPage() {
   const { hash } = useLocation();
-  const TOKEN = hash.slice(14, 231);
 
   useEffect(() => {
+    const token = queryString.parse(hash).access_token;
+
     const exchangeGoogleAccessToken = async () => {
-      const body = {
-        grant_type: "convert_token",
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET,
-        backend: "google-oauth2",
-        token: TOKEN,
-      };
-
-      const requestOptions = {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-        body: JSON.stringify(body),
-      };
-
       try {
-        const response = await fetch(CONVERT_TOKEN_URL, requestOptions);
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = await response.json();
-
-        const getArticles = async () => {
-          const requestOptions = {
-            headers: {
-              Authorization: `Bearer ${data.access_token}`,
-            },
-            method: "GET",
-          };
-
-          try {
-            const response = await fetch(GET_ARTICLES_URL, requestOptions);
-            const data = await response.json();
-            console.log("Data:", data);
-          } catch (error) {
-            console.error("Error during load articles:", error);
-          }
+        const body = {
+          grant_type: "convert_token",
+          client_id: CLIENT_ID,
+          client_secret: CLIENT_SECRET,
+          backend: "google-oauth2",
+          token,
         };
-        getArticles();
+        const convertTokenRequestOptions = {
+          body: JSON.stringify(body),
+          headers: {
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+        };
+        const fetchTokenConversion = await fetch(
+          CONVERT_TOKEN_URL,
+          convertTokenRequestOptions
+        );
+        if (!fetchTokenConversion.ok) {
+          throw new Error(`HTTP error! Status: ${fetchTokenConversion.status}`);
+        }
+        const tokenData = await fetchTokenConversion.json();
+
+        const getArticlesRequestOptions = {
+          headers: {
+            Authorization: `Bearer ${tokenData.access_token}`,
+          },
+          method: "GET",
+        };
+        const fetchArticles = await fetch(
+          GET_ARTICLES_URL,
+          getArticlesRequestOptions
+        );
+        const articlesData = await fetchArticles.json();
+        console.log("Data:", articlesData);
       } catch (error) {
-        console.error("Error during exchange google access token:", error);
+        console.error("Error during fetch data:", error);
       }
     };
     exchangeGoogleAccessToken();
-  }, [TOKEN]);
+  }, [hash]);
 
   return null;
 }
